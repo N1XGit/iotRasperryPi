6be11be239fae42796ffa7bf451722fc9b8e4dbe
N1XGit <niilo.johannes.nk@gmail.com>
2025-01-17 13:06:55 +0200

    Update PeliTesti.py

--- grovelcd/PeliTesti.py
+++ grovelcd/PeliTesti.py
@@ -2,7 +2,7 @@ from base import *
 from i2c import Bus
 import time
 import sys
-import keyboard
+from pynput import keyboard
 
 # sphinx autoapi required
 __all__ = ["JHD1802"]
@@ -66,6 +66,21 @@ class JHD1802(Display):
     def textCommand(self, cmd):
         self._bus.write_byte_data(self._addr,0x80,cmd)
         
+
+
+def on_press(key):
+    if key == keyboard.Key.esc:
+        return False  # stop listener
+    try:
+        k = key.char  # single-char keys
+    except:
+        k = key.name  # other keys
+    if k in ['space']:  # keys of interest
+        # self.keys.append(k)  # store it in global-like variable
+        print('Key pressed: ' + k)
+        return False  # stop listener; remove this if want more keys
+
+
 
 Width = 16
 Height = 2
