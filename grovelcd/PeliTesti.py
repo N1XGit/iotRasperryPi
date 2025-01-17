5db67f52f43d90e1acf502f31c016cf38a89d6dc
N1XGit <niilo.johannes.nk@gmail.com>
2025-01-17 13:43:36 +0200

    Update PeliTesti.py

--- grovelcd/PeliTesti.py
+++ grovelcd/PeliTesti.py
@@ -92,7 +92,7 @@ def print_game(PlayerPosX:int, PlayerPosY:int, ObstaclePos:int, score:int): # va
             else:
                 lcd.write('O' + ' ' * (ObstaclePos - 2) + 'x' + ' ' * (Width - ObstaclePos)) 
     for i in range(cols):
-        lcd.write(chr(i)
+        lcd.write(chr(i))
 
 def main():
     global PlayerPositionX, ObstaclePos, PlayerPositionY, score, counter
