7457bd1e28729f9e6c2b3b3d68d8c11f5d5fc93f
N1XGit <niilo.johannes.nk@gmail.com>
2025-01-24 11:06:27 +0200

    !

--- grovelcd/PeliUpdate1.py
+++ grovelcd/PeliUpdate1.py
@@ -88,7 +88,7 @@ def getName():
         time.sleep(2)
 
         lcd.clear()
-        lcd.setCursor(0, 1)
+        lcd.setCursor(0, 0)
         lcd.write("Save name? y/n")
 
         lcd.setCursor(1, 0)
